a6f248365f356a6ca066593524ee51aa3c981c8a
Nederlandt Nicolas <nicolas.nederlandt@student.vinci.be>
2023-08-28 06:40:10 +0200

    FT01.5 text section

--- tuto1/lib/main.dart
+++ tuto1/lib/main.dart
@@ -67,6 +67,20 @@ class MyApp extends StatelessWidget {
     );
     // #enddocregion buttonSection
 
+    // #docregion textSection
+    Widget textSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: const Text(
+        'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
+            'Alps. Situated 1,578 meters above sea level, it is one of the '
+            'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
+            'half-hour walk through pastures and pine forest, leads you to the '
+            'lake, which warms to 20 degrees Celsius in the summer. Activities '
+            'enjoyed here include rowing, and riding the summer toboggan run.',
+        softWrap: true,
+      ),
+    );
+    // #enddocregion textSection
 
     return MaterialApp(
       title: 'Welcome to Flutter',
@@ -78,6 +92,7 @@ class MyApp extends StatelessWidget {
           children: [
             titleSection,
             buttonSection,
+            textSection,
           ],
         ),
       ),
