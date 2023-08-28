783572b15082ecfefcd67269e13e8c2df8d6221d
Nederlandt Nicolas <nicolas.nederlandt@student.vinci.be>
2023-08-29 01:48:05 +0200

    FT04.7 Consumer

--- ftuto4/lib/views/second_screen.dart
+++ ftuto4/lib/views/second_screen.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:ftuto4/view_model/click_view_model.dart';
+import 'package:provider/provider.dart';
 
 class SecondScreen extends StatelessWidget {
   const SecondScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-
-    final nbClicks = ModalRoute.of(context)!.settings.arguments as int;
 
     return Scaffold(
       appBar: AppBar(
@@ -24,7 +24,11 @@ class SecondScreen extends StatelessWidget {
           children: [
             const Text("Hello from second screen"),
             const SizedBox(height: 16.0),
-            Text("There were $nbClicks clicks in the first page"),
+            Consumer<ClickViewModel>(
+              builder: (context, viewModel, child) =>
+                Text("There were ${viewModel.clicks} clicks in the first page"),
+            ),
+
           ],
         ),
       ),
