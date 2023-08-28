0e516055a15c8b623267c68f4ec95a381ae197cc
Nederlandt Nicolas <nicolas.nederlandt@student.vinci.be>
2023-08-28 22:24:05 +0200

    F04.1 Représentation des données

---
+++ ex3/lib/note.dart
@@ -0,0 +1,15 @@
+class Note {
+  final String title;
+  final String text;
+
+  Note({required this.title, required this.text});
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is Note && runtimeType == other.runtimeType && title == other.title;
+
+  @override
+  int get hashCode => title.hashCode;
+}
+
