df4f3fc38d978124f99365ac30e4b336e866b11c
Nederlandt Nicolas <nicolas.nederlandt@student.vinci.be>
2023-08-28 10:46:31 +0200

    F03.1 Sélection de mode

--- ex2/lib/main.dart
+++ ex2/lib/main.dart
@@ -5,23 +5,47 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
 
+class _MyAppState extends State<MyApp> {
+
+  var showState = "Sell";
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
 
     final propertiesToDisplay = [
       for(var property in defaultProperties)
-        property
+        if(property.state == showState) property
+
     ];
     return MaterialApp(
       title: 'Flutter Demo',
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Properties"),
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text("To sell"),
+              Switch(
+                  value: showState == "Sell",
+                  onChanged: (bool value) {
+                    setState(() {
+                      if(showState == "Sell") {
+                        showState = "Loan";
+                      } else {
+                        showState = "Sell";
+                      }
+                    });
+                  }),
+              const Text("To loan"),
+            ],
+          ),
         ),
         body:
             ListView.builder(
