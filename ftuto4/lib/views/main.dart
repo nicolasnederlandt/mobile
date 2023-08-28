dc457dc2b2beb28661902f309625679d43d06afc
Nederlandt Nicolas <nicolas.nederlandt@student.vinci.be>
2023-08-29 01:41:29 +0200

    FT04.5 ChangeNotifiderProvider

--- ftuto4/lib/views/main.dart
+++ ftuto4/lib/views/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:ftuto4/view_model/click_view_model.dart';
 import 'package:ftuto4/views/first_screen.dart';
 import 'package:ftuto4/views/second_screen.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,16 +14,18 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter démo',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      initialRoute: "/first",
-      routes: {
-        "/first": (context) => const FirstScreen(),
-        "/second": (context) => const SecondScreen(),
-      }
-      );
-
+    return ChangeNotifierProvider<ClickViewModel>(
+        create: (context) => ClickViewModel(),
+        child:  MaterialApp(
+          title: 'Flutter démo',
+          theme: ThemeData(primarySwatch: Colors.blue),
+          initialRoute: "/first",
+          routes: {
+            "/first": (context) => const FirstScreen(),
+            "/second": (context) => const SecondScreen(),
+         }
+        ),
+    );
   }
 }
 
