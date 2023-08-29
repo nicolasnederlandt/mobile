bb9c489b983bf1a754cf13314e475c8822f7a63f
Nederlandt Nicolas <nicolas.nederlandt@student.vinci.be>
2023-08-29 04:10:05 +0200

    FT05.2 Parsing JSON

--- ftuto5/lib/view/home_screen.dart
+++ ftuto5/lib/view/home_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:url_launcher/url_launcher.dart';
+
+import '../model/film.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -8,41 +11,67 @@ class HomeScreen extends StatefulWidget {
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
-
+enum FetchState { loading, error, done}
 class _HomeScreenState extends State<HomeScreen> {
 
-  String result = "Click on the button to launch the request";
+  var state = FetchState.loading;
+  var message = "Loading...";
+  Film? film;
 
   Future<void> _initFilm() async {
-    const url = "https://sebstreb.github.io/flutter-fiche-5/films-api/1";
     try {
-      setState(() => result = "Loading, please wait ...");
-      var response = await http.get(Uri.parse(url));
-      setState(() => result = response.body);
-    } catch(error) {
-      setState( () => result = error.toString());
+      var response = await Film.fetchFilm(2);
+      setState( () {
+        state = FetchState.done;
+        film = response;
+      });
+    } catch (error) {
+      setState( () {
+        state = FetchState.error;
+        message = error.toString();
+      });
     }
   }
+
+  @override
+  void initState() {
+    super.initState();
+    _initFilm();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Tutoriel 5")),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            Expanded(
-                child: Center(
-                  child: Text(result),
-                ),
-            ),
-            ElevatedButton(
-              onPressed: _initFilm,
-              child: const Text("Fecth movie n°1"),
-            )
-          ],
+        child: state != FetchState.done
+          ? Column( children: [ Center( child: Text(message),)])
+          : FilmRow(film: film!),
         ),
-      )
+      );
+
+  }
+}
+class FilmRow extends StatelessWidget {
+  final Film film;
+
+  const FilmRow({Key, key, required this.film}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(film.title),
+      subtitle: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text("Directed by ${film.director} - ${film.duration} minutes"),
+          InkWell(
+            onTap: () => launchUrl(Uri.parse(film.link)),
+            child: Text(film.link),
+          )
+        ],
+      ),
     );
   }
 }
